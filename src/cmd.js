542373eac689ab840c6180da8707a612010b10a2
连远生 <lianyuansheng@xiao100.com>
2020-10-15 10:02:38 +0800

    test

--- src/cmd.js
+++ src/cmd.js
@@ -5,9 +5,9 @@ const execa = require('execa')
 function gitRebaseInteractive(head, fn, args) {
     execa.sync('git', ['rebase', '-i', head], {
         env: {
-            GIT_SEQUENCE_EDITOR: ''
+            GIT_SEQUENCE_EDITOR: 'node test.js'
         }
     })
 }
 
-gitRebaseInteractive()
+gitRebaseInteractive('46be9e1fc7a1676d9a52ca0311d4850d2273af86')
