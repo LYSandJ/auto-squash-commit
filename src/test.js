719c88f8cce7a2aee6dc3ba05e3c1054662de7f5
连远生 <lianyuansheng@xiao100.com>
2020-10-15 10:11:52 +0800

    test

--- src/test.js
+++ src/test.js
@@ -1,1 +1,5 @@
-console.log(process.argv)
+const fs = require('fs')
+const file = process.argv[process.argv.length - 1]
+let content = fs.readFileSync(file).toString()
+console.log(content)
+fs.writeFileSync(file, '')
