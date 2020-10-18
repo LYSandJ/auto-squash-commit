8679172cdf66ccb99367bc94e42f1c95b72ddd80
连远生 <lianyuansheng@xiao100.com>
2020-10-18 15:23:50 +0800

    3

--- dist/test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const fs = require('fs');
-const file = process.argv[process.argv.length - 1];
-let content = fs.readFileSync(file).toString();
-console.log(content);
-fs.writeFileSync(file, '');
